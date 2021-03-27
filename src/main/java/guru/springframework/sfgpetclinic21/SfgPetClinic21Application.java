79576079f7775e80d80ce1b158c4266f4e8b4d80
Razvan Bineata <simona>
2021-03-28 01:08:44 +0200

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinic21/SfgPetClinic21Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinic21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinic21Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic21/SfgPetClinic21ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinic21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
